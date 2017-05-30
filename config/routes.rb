93dd6a4172abb11c983fa5ffea044e7c62f7e1b7
Isis Tejeda <isis@isistejeda.com>
2017-05-30 15:38:33 -0500

    session controller created, basic layout for sign in done

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   resources :users, only:[:new,:create]
 
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  delete '/signout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
