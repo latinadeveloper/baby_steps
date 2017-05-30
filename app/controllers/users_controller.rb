e77bdcea519461d800954c15d3ae723f409f8bf1
Isis Tejeda <isis@isistejeda.com>
2017-05-30 13:47:20 -0500

    password confirmation/validations added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
-        redirect_to @user
+        redirect_to root_path
       else
         render "new"
       end
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-      params.require(:user).permit(:name, :password)
+      params.require(:user).permit(:name, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence:true
+  validates :name, presence:true, uniqueness: true, length: { minimum: 4 }
+  validates :password, length: { in: 6..20 }
+  validates :password_confirmation, presence: true
   has_secure_password
 end
