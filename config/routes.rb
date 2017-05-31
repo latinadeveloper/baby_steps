3cc78ff123ea40384ebdf7384cae99b987f28858
Isis Tejeda <isis@isistejeda.com>
2017-05-30 21:55:44 -0500

    children controller, nested route added

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   post '/signin' => 'sessions#create'
   delete '/signout' => 'sessions#destroy'
 
+  resources :children do
+    resources :skills, only: [:index]
+  end
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
