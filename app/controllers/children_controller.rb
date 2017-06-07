06cfff55a281559c6d5f342191ac7355d7441f1f
Isis Tejeda <isis@isistejeda.com>
2017-06-07 11:30:44 -0500

    child controller removed, update action added to children controller

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,2 +1,18 @@
 class ChildrenController < ApplicationController
+  def update
+    @child = Child.find_by(id: params[:id])
+    @child.update(child_params)
+  end
+
+
+  private
+  def child_params
+    params.require(:child).permit(
+      accomplishments_attributes: [
+      # :skill_id,
+      # :perform
+      :comment
+      ]
+      )
+  end
 end
