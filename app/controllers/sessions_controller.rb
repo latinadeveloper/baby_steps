4517ef62e35d7a5870c60608fb13831164d356e5
Isis Tejeda <isis@isistejeda.com>
2017-06-10 22:49:13 -0500

    added log in link for facebook, trying to make it work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create  # it does the sign in
-    @user = User.find_by(name: params[:user][:name])
-    if @user
-      @verify = @user.authenticate(params[:user][:password])
-    end
-    if @verify
-      session[:user_id] = @user.id
-      flash[:notice] = "Welcome, you've logged in."
+    if request.env['omniauth.auth']
+      user = User.create_with_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
       redirect_to home_path
     else
-      flash[:error] = "Wrong username or password."
-      redirect_to signin_path
+      @user = User.find_by(name: params[:user][:name])
+      if @user
+        @verify = @user.authenticate(params[:user][:password])
+      end
+      if @verify
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome, you've logged in."
+        redirect_to home_path
+      else
+        flash[:error] = "Wrong username or password."
+        redirect_to signin_path
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,9 @@ Rails.application.routes.draw do
   end
 
 
+  get 'auth/facebook/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
