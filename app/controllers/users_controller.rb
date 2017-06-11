6fb038846f66a01447c64dc9f5a329186323628c
Isis Tejeda <isis@isistejeda.com>
2017-06-10 21:03:40 -0500

    make sure user logged in after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome, you've logged in."
         redirect_to home_path
       else
         render 'new'
