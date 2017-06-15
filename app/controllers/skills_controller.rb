65bdff7fd4a89ab3ea0eb0b44fb6182209c6b72d
Isis Tejeda <isis@isistejeda.com>
2017-06-15 09:02:32 -0500

    accomplishment params attribute fo perfom added so checkbox remains after submition

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -43,6 +43,6 @@ class SkillsController < ApplicationController
   end
 
   def accomplishment_params
-    params.require(:accomplishment).permit(:skill_id, :comment)
+    params.require(:accomplishment).permit(:skill_id, :comment, :perform)#attribute wasnot permitted
   end
 end
